d0f20fbeeb5b3d734dcb1e6466cb5d1686fb849e
Li <854378082@qq.com>
2016-02-03 09:27:47 +0800

    自动播放动画插件-优化索引选择

--- js/com.js
+++ js/com.js
@@ -6,41 +6,100 @@
 	2、将元素对应事件绑定到插件上，并设置执行间隔
 	3、重点在zd()这个函数中，他是取当前的hover并判断下一步执行哪个内容
 */
+/*
+	自动播放动画插件
+*/
 (function(){
+	// 定义当前在播放动画的timeout
+	var t1 = t2 = t3 = null;
+	// 公用方法或者变量
 	var inits = function(opt)
-	{
+	{	
+		// 默认值
 		this.defaults = {
 			// 主内容元素
-			obj:'',
+			obj:'.tab_div',
 			// 焦点元素
-			hovers:'',
+			hovers:'.tab_t span',
 			// 焦点元素类名
 			focusClass:'hover',
-			times:1500
+			// 播放时间
+			inTime:[],
+			// 出场时间
+			outTime:[],
+			// 入场动画
+			inDh:[],
+			// 出场动画
+			outDh:[],
 		},
 		// 用户定义值取代默认值
 		this.options = $.extend(this.defaults,opt),
-		this.zd = function(){
-			// 找出当前索引
-			var showIndex = $(this.options.obj + "." + this.options.focusClass).index();
-			var objLength = $(this.options.obj).length;
-			// 如果不是最后一个，跳转显示下一个，如果已经是最后一个，显示第一个
-			if (showIndex < objLength - 1 )
-			{
-				$(this.options.obj).eq(showIndex + 1).trigger('click');
-			}
-			else
-			{
-				$(this.options.obj).eq(0).trigger('click');
-			}
-		}
+		this.objLength = $(this.options.obj).length
 	};
-	$.fn.li_tabs = function(opt)
-	{
-		var init = new inits(opt);
-		setInterval(function(){
-			init.zd();
-		}, init.options.times);
+	// 自动播放功能
+	inits.prototype = {
+		zd: function(){
+				// 把this指向inits，用来在子func中取opt
+				var init = this;
+				// 清空定时内容
+				clearTimeout(t1);
+				clearTimeout(t2);
+				clearTimeout(t3);
+				// 每次重置
+				t1 = t2 = t3 = null;
+				// 找出当前索引
+				var showIndex = $(init.options.hovers + '.' + init.options.focusClass).index();
+				// 执行当前场次入场
+				init.options.inDh[showIndex]();
+				// 出场
+				t1 = setTimeout(function(){
+					init.options.outDh[showIndex]();
+				},init.options.inTime[showIndex]);
+				// 设置等等时间
+				t2 = setTimeout(function(){
+					// 如果不是最后一个，跳转显示下一个，如果已经是最后一个，显示第一个
+					if (showIndex < init.objLength - 1 )
+					{
+						$(init.options.obj).eq(showIndex + 1).show().siblings().hide();
+						$(init.options.hovers).eq(showIndex + 1).addClass(init.options.focusClass).siblings().removeClass(init.options.foucsClass);
+					}
+					else
+					{
+						$(init.options.obj).eq(0).show().siblings().hide();
+						$(init.options.hovers).eq(0).addClass(init.options.foucsClass).siblings().removeClass(init.options.foucsClass);
+					}
+					// 调用自身，用来循环
+					init.zd();
+				},init.options.inTime[showIndex] + init.options.outTime[showIndex]);
+			}
+	};
+	$.fn.li_tabs_dh = function(options){
+		var init = new inits(options);
+		// 初始化显示
+		$(init.options.obj).hide().first().show();
+		// 自动播放开始
+		init.zd();
+		// 点击切换功能
+		$(init.options.hovers).bind('click',function(){
+			// 开始手动切换，点击的索引
+			var TI = $(this).index();
+			// 找出当前索引
+			var showIndex = $(init.options.hovers + '.' + init.options.focusClass).index();
+			// 执行当前场次出场
+			// 清空定时内容
+			clearTimeout(t1);
+			clearTimeout(t2);
+			clearTimeout(t3);
+			t1 = t2 = t3 = null;
+			init.options.outDh[showIndex]();
+			t3 = setTimeout(function(){
+				$(this).addClass(init.options.foucsClass).siblings().removeClass(init.options.foucsClass);
+				$(init.options.obj).eq(TI).show().siblings().hide();
+				$(init.options.hovers).eq(TI).addClass(init.options.foucsClass).siblings().removeClass(init.options.foucsClass);
+				init.zd();
+			},init.options.outTime[showIndex]);
+			// 再次开始自动切换
+		});
 	}
 })(jQuery);
 /*
